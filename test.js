da98c223facd8cdc83591fcf1173630e31031efe
yukiroow <hcdominguez07@gmail.com>
2026-01-12 13:59:14 +0800

    Add test for new endpoint

--- test.js
+++ test.js
@@ -1,12 +1,22 @@
 const test = require("node:test");
 const assert = require("node:assert/strict");
 
-test("Response JSON test", async (t) => {
+test("Test root endpoint", async (t) => {
     const res = await fetch("http://127.0.0.1:7777");
     const body = await res.text();
     assert.strictEqual(
         JSON.parse(body).message,
-        "Hello There!",
+        "Hello There! ",
         "Response should have a variable `message` with value `Hello There!`"
+    );
+});
+
+test("Test /goodbye endpoint", async (t) => {
+    const res = await fetch("http://127.0.0.1:7777/goodbye");
+    const body = await res.text();
+    assert.strictEqual(
+        JSON.parse(body).message,
+        "Bye bye!",
+        "Response should have a variable `message` with value `Bye bye!`"
     );
 });
