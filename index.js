08a525988aea3fd80f1a066103cde1de45e59fa1
yukiroow <hcdominguez07@gmail.com>
2026-01-12 13:57:47 +0800

    Add new endpoint

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const http = require("http");
+const url = require("url");
 
 const server = http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+    const pathname = parsedUrl.pathname;
+
+    if (pathname === "/goodbye") {
+        const message = { message: "Bye bye!" };
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(message));
+        return;
+    }
+
     const message = { message: "Hello There!" };
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(JSON.stringify(message));
