09bceeacbde36b3b72b7026aa39060e0a2347412
yukiroow <hcdominguez07@gmail.com>
2026-01-12 14:06:40 +0800

    Update test URL

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const test = require("node:test");
 const assert = require("node:assert/strict");
 
 test("Response JSON test", async (t) => {
-    const res = await fetch("http://127.0.0.1:7777");
+    const res = await fetch("http://localhost:7777");
     const body = await res.text();
     assert.strictEqual(
         JSON.parse(body).message,
