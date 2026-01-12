41449e0a977721bdebecabde039b81f234892e53
yukiroow <hcdominguez07@gmail.com>
2026-01-12 13:23:35 +0800

    Add simple HTTP server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    const message = { message: "Hello There!!" };
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(message));
+});
+
+server.listen(7777, () => {
+    console.log("Server has started at port 7777");
+});
