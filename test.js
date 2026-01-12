e753c922a6702c2baa6328661d8dc12ab6273a9e
yukiroow <hcdominguez07@gmail.com>
2026-01-12 13:36:51 +0800

    Add test file

---
+++ test.js
@@ -0,0 +1,12 @@
+const test = require("node:test");
+const assert = require("node:assert/strict");
+
+test("Response JSON test", async (t) => {
+    const res = await fetch("http://127.0.0.1:7777");
+    const body = await res.text();
+    assert.strictEqual(
+        JSON.parse(body).message,
+        "Hello There! ",
+        "Response should have a variable `message` with value `Hello There!`"
+    );
+});
