e7126dd6e409bed2a88fca89746bc2a48df65fd7
yukiroow <hcdominguez07@gmail.com>
2026-01-13 14:57:34 +0800

    Separate start and stopping of server

--- test.js
+++ test.js
@@ -3,9 +3,11 @@ const assert = require("node:assert/strict");
 const server = require("./index.js");
 const http = require("http");
 
-test("Response JSON test", async (t) => {
+test.before(async () => {
     await new Promise((resolve) => server.listen(0, resolve));
-    console.log(server.address());
+});
+
+test("Response JSON test", async (t) => {
     const { port } = server.address();
     const res = await fetch(`http://localhost:${port}`);
     const body = await res.text();
@@ -14,5 +16,8 @@ test("Response JSON test", async (t) => {
         "Hello There!",
         "Response should have a variable `message` with value `Hello There!`"
     );
+});
+
+test.after(async () => {
     await new Promise((resolve) => server.close(resolve));
 });
