42f57980794aec747533beeb3048ec7636e741d3
yukiroow <hcdominguez07@gmail.com>
2026-01-19 16:54:28 +0800

    Fix spacing issue

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    const message = { message: "Hello There! " };
+    const message = { message: "Hello There!" };
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(JSON.stringify(message));
 });
